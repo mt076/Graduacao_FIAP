ec1049551bab84c81bf4bbee2f437036dc4983ff
Matheus Dos Anjos <matheus20x60@gmail.com>
2026-09-05 18:29:16 -0300

    Script e README.md finalizado

--- Python- FIAP_RASCUNHO/algoritmo_aluno_aprovado.py
+++
@@ -1,8 +0,0 @@
-notas_cursos = {"engenharia_prompt": 10, "matematica": 4, "sustentabilidade": 10, "programacao": 8.7}
-
-media_aprovacao = (notas_cursos["engenharia_prompt"] + notas_cursos["matematica"] + notas_cursos["sustentabilidade"] + notas_cursos["programacao"]) / 4
-
-if media_aprovacao >= 6:
-    print(f"Aluno aprovado! Média final: {media_aprovacao :.2f}")
-else:
-    print(f"Aluno reprovado! Média final: {media_aprovacao :.2f}")
--- Python- FIAP_RASCUNHO/tipos_variaveis.py
+++
@@ -1,7 +0,0 @@
-ano = 1998
-nome = "Matheus dos Anjos"
-saldo = 98.300
-
-print(("O tipo de variável ano é {}""".format(type(ano))))
-print(("O tipo de variável nome é {}".format(type(nome))))
-print(("O tipo de variável saldo é {}".format(type(saldo))))
